9eb0cb5567c0f4c0800276f7d190e0ee0937ec8a
xNissyx <126715665+xNissyx@users.noreply.github.com>
2023-04-18 14:59:03 +0000

    [finish] Advanced 4

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,24 @@
+class RelationshipsController < ApplicationController
+
+  def create
+    followed_user = User.find(params[:user_id])
+    current_user.follow(followed_user)
+    redirect_back fallback_location: root_path
+  end
+
+  def destroy
+    followed_user = User.find(params[:user_id])
+    current_user.unfollow(followed_user)
+    redirect_back fallback_location: root_path
+  end
+  
+  def followings
+    user = User.find(params[:user_id])
+    @following_users = user.followings
+  end
+  
+  def followers
+    user = User.find(params[:user_id])
+    @follower_users = user.followers
+  end
+end
